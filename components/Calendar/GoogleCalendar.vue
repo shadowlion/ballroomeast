23ca532f074ee8817b0f300950054fd0c256e17c
Justin Chiou <chiou.kai@gmail.com>
2019-10-08 19:25:39 -0400

    use native b-embed to do some cool stuff!

--- components/Calendar/GoogleCalendar.vue
+++ components/Calendar/GoogleCalendar.vue
@@ -1,11 +1,16 @@
 <template lang="pug" functional>
-  .text-center#google-calendar
-    iframe(
-      src="https://calendar.google.com/calendar/embed?src=ballroomeast.com_tf7ohgpna1q96c47droquq7e30%40group.calendar.google.com&ctz=America%2FNew_York"
-      style="border-width:0"
-      frameborder="0"
-      scrolling="no"
+  .text-center
+    b-embed(
+      type="iframe"
+      aspect="16by9"
+      src="https://calendar.google.com/calendar/embed?src=ballroomeast.com_tf7ohgpna1q96c47droquq7e30%40group.calendar.google.com&ctz=America%2FNew_York&mode=AGENDA"
     )
+    //- iframe(
+    //-   src="https://calendar.google.com/calendar/embed?src=ballroomeast.com_tf7ohgpna1q96c47droquq7e30%40group.calendar.google.com&ctz=America%2FNew_York"
+    //-   style="border-width:0"
+    //-   frameborder="0"
+    //-   scrolling="no"
+    //- )
 </template>
 
 <script>
@@ -13,14 +18,3 @@ export default {
   functional: true
 };
 </script>
-
-<style scoped>
-#google-calendar {
-  height: 600px;
-}
-
-iframe {
-  width: 100%;
-  height: 600px;
-}
-</style>
