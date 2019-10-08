f1eb5ab18d8375cdd3e541c89ef0832240942860
Justin Chiou <chiou.kai@gmail.com>
2019-10-08 12:47:18 -0400

    make google calendar a functional component

--- components/Calendar/GoogleCalendar.vue
+++ components/Calendar/GoogleCalendar.vue
@@ -1,4 +1,4 @@
-<template lang="pug">
+<template lang="pug" functional>
   .text-center#google-calendar
     iframe(
       src="https://calendar.google.com/calendar/embed?src=ballroomeast.com_tf7ohgpna1q96c47droquq7e30%40group.calendar.google.com&ctz=America%2FNew_York"
@@ -6,19 +6,13 @@
       frameborder="0"
       scrolling="no"
     )
-    //- iframe.d-none.d-md-block(
-    //-   src="https://calendar.google.com/calendar/embed?src=ballroomeast.com_tf7ohgpna1q96c47droquq7e30%40group.calendar.google.com&ctz=America%2FNew_York&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1"
-    //-   style="border-width:0"
-    //-   frameborder="0"
-    //-   scrolling="no"
-    //- )
-    //- iframe.d-block.d-md-none(
-    //-   src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=YmFsbHJvb21lYXN0LmNvbV90ZjdvaGdwbmExcTk2YzQ3ZHJvcXVxN2UzMEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23B39DDB&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1&amp;mode=AGENDA"
-    //-   style="border-width:0"
-    //-   frameborder="0"
-    //-   scrolling="no"
-    //- )
 </template>
+
+<script>
+export default {
+  functional: true
+};
+</script>
 
 <style scoped>
 #google-calendar {
