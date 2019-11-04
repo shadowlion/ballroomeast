35770da64db7d8e87438b513bbeeae2b92b96def
Justin Chiou <chiou.kai@gmail.com>
2019-11-04 08:46:14 -0500

    remove commented items

--- components/Calendar/GoogleCalendar.vue
+++ components/Calendar/GoogleCalendar.vue
@@ -9,12 +9,6 @@
           src="https://calendar.google.com/calendar/embed?src=ballroomeast.com_tf7ohgpna1q96c47droquq7e30%40group.calendar.google.com&ctz=America%2FNew_York&mode=AGENDA"
         )
       b-col(lg="2").d-none.d-lg-block
-    //- iframe(
-    //-   src="https://calendar.google.com/calendar/embed?src=ballroomeast.com_tf7ohgpna1q96c47droquq7e30%40group.calendar.google.com&ctz=America%2FNew_York"
-    //-   style="border-width:0"
-    //-   frameborder="0"
-    //-   scrolling="no"
-    //- )
 </template>
 
 <script>
