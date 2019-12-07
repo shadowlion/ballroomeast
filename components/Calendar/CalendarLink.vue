7559d21f9f16dc44640da8cfc42e0a301b6478eb
Justin Chiou <chiou.kai@gmail.com>
2019-12-06 22:37:12 -0500

    reduce lines of code using interpolation

--- components/Calendar/CalendarLink.vue
+++ components/Calendar/CalendarLink.vue
@@ -1,24 +1,10 @@
 <template lang="pug" functional>
   .row.pt-5.pb-2
     .col.text-center
-      p Add this
-        |
-        |
-        a(href="https://calendar.google.com/calendar?cid=YmFsbHJvb21lYXN0LmNvbV90ZjdvaGdwbmExcTk2YzQ3ZHJvcXVxN2UzMEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t") Google Calendar
-        |
-        |
-        | to your account.
-      p Need an offline version?
-        |
-        |
-        a(href="/calendar.pdf"
-          target="_blank"
-          download
-        ) Download the PDF.
+      p Add this #[a(href="https://calendar.google.com/calendar?cid=YmFsbHJvb21lYXN0LmNvbV90ZjdvaGdwbmExcTk2YzQ3ZHJvcXVxN2UzMEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t") Google Calendar] to your account.
+      p Need an offline version? #[a(href="/calendar.pdf" target="_blank" download) Download the PDF].
 </template>
 
 <script>
-export default {
-  functional: true
-};
+export default { functional: true };
 </script>
