8b8d4ae325c4cd20ead1c6ab585b13b71935e5c2
Justin Chiou <chiou.kai@gmail.com>
2019-12-10 15:30:04 -0500

    add 2020 calendar

--- components/Calendar/CalendarLink.vue
+++ components/Calendar/CalendarLink.vue
@@ -2,7 +2,10 @@
   .row.pt-5.pb-2
     .col.text-center
       p Add this #[a(href="https://calendar.google.com/calendar?cid=YmFsbHJvb21lYXN0LmNvbV90ZjdvaGdwbmExcTk2YzQ3ZHJvcXVxN2UzMEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t") Google Calendar] to your account.
-      p Need an offline version? #[a(href="/calendar.pdf" target="_blank" download) Download the PDF].
+      p Need an offline version?
+      a(href="/2019.pdf" target="_blank" download) 2019 Calendar
+      br
+      a(href="/2020.pdf" target="_blank" download) 2020 Calendar
 </template>
 
 <script>
