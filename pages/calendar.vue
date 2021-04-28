d56a729b61820e7f896da17b082b2929b2112dba
Justin Chiou <chiou.kai@gmail.com>
2021-04-28 16:54:24 -0400

    refactor: move calendar components to page

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,8 +1,30 @@
 <template>
-  <section id="calendar">
+  <article id="calendar">
     <div class="container py-5">
-      <google-calendar></google-calendar>
-      <calendar-link></calendar-link>
+      <div class="row text-center justify-content-center">
+        <div class="col-md-8">
+          <b-embed
+            type="iframe"
+            aspect="1by1"
+            src="https://calendar.google.com/calendar/embed?src=ballroomeast.com_tf7ohgpna1q96c47droquq7e30%40group.calendar.google.com&ctz=America%2FNew_York&mode=AGENDA"
+          />
+        </div>
+      </div>
+      <div class="row pt-5 pb-2">
+        <div class="col text-center">
+          <p>
+            Add this
+            <a
+              href="https://calendar.google.com/calendar?cid=YmFsbHJvb21lYXN0LmNvbV90ZjdvaGdwbmExcTk2YzQ3ZHJvcXVxN2UzMEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
+            >
+              Google Calendar
+            </a>
+            to your account.
+          </p>
+          <p>Need an offline version?</p>
+          <a href="/2020.pdf" target="_blank" download>2020 Calendar</a>
+        </div>
+      </div>
     </div>
-  </section>
+  </article>
 </template>
