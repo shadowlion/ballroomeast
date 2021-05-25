9fea755614e57ca5983135ae0d4ab0a58abc8c4f
Justin Chiou <chiou.kai@gmail.com>
2021-05-24 17:55:27 -0700

    fix: remove Monday/Tuesday classes

--- pages/lessons.vue
+++ pages/lessons.vue
@@ -22,7 +22,7 @@
           <div
             v-for="(lesson, index) in lessons"
             :key="index"
-            class="col-md-6 col-lg-4"
+            class="col-md-12 col-lg-6"
           >
             <LessonCard :lesson="lesson" />
           </div>
@@ -46,34 +46,6 @@ export default Vue.extend({
           description:
             "All Beginner Group Classes are open to the public and usually consists of 16 to 55 people. Beginners especially like the opportunity to meet and dance with other novice dancers and are given the opportunity to rotate partners frequently throughout the group lesson (or stay with one partner if they wish - no pressure!). This lesson is the perfect blend of instruction and social ambiance.",
           day: "Wednesdays",
-          rate: 5,
-          hasLink: true,
-          link: {
-            title: "Check the Calendar",
-            url: "/calendar",
-          },
-        },
-        {
-          imgUrl:
-            "https://firebasestorage.googleapis.com/v0/b/ballroom-east.appspot.com/o/public%2Fintermediate.jpg?alt=media&token=d16f98e8-a3cc-49f9-ab5c-f740e4d3c072",
-          title: "Group - Intermediate",
-          description:
-            "Once you finish a month of Beginner Group Classes you can move into the Tuesday night Intermediate Group Classes and continue learning the same dance for another month. This gives you the opportunity to learn more steps and more techniques making you a better dancer in that particular dance.",
-          day: "Tuesdays",
-          rate: 5,
-          hasLink: true,
-          link: {
-            title: "Check the Calendar",
-            url: "/calendar",
-          },
-        },
-        {
-          imgUrl:
-            "https://firebasestorage.googleapis.com/v0/b/ballroom-east.appspot.com/o/public%2Fadvanced.jpg?alt=media&token=ae88e736-5a06-4991-bb38-fc634b09e7b9",
-          title: "Group - Advanced",
-          description:
-            "After you have gone through the Beginner and Intermediate Group Classes, our Advanced Class is perfect for you if you would like to learn some more complicated figures and advanced techniques. Building off the material taught in our Intermediate and Beginner Classes, the Advanced Group Class provides a chance practice your skills with a diversity of moderately skilled partners.",
-          day: "Mondays",
           rate: 10,
           hasLink: true,
           link: {
@@ -81,6 +53,34 @@ export default Vue.extend({
             url: "/calendar",
           },
         },
+        // {
+        //   imgUrl:
+        //     "https://firebasestorage.googleapis.com/v0/b/ballroom-east.appspot.com/o/public%2Fintermediate.jpg?alt=media&token=d16f98e8-a3cc-49f9-ab5c-f740e4d3c072",
+        //   title: "Group - Intermediate",
+        //   description:
+        //     "Once you finish a month of Beginner Group Classes you can move into the Tuesday night Intermediate Group Classes and continue learning the same dance for another month. This gives you the opportunity to learn more steps and more techniques making you a better dancer in that particular dance.",
+        //   day: "Tuesdays",
+        //   rate: 5,
+        //   hasLink: true,
+        //   link: {
+        //     title: "Check the Calendar",
+        //     url: "/calendar",
+        //   },
+        // },
+        // {
+        //   imgUrl:
+        //     "https://firebasestorage.googleapis.com/v0/b/ballroom-east.appspot.com/o/public%2Fadvanced.jpg?alt=media&token=ae88e736-5a06-4991-bb38-fc634b09e7b9",
+        //   title: "Group - Advanced",
+        //   description:
+        //     "After you have gone through the Beginner and Intermediate Group Classes, our Advanced Class is perfect for you if you would like to learn some more complicated figures and advanced techniques. Building off the material taught in our Intermediate and Beginner Classes, the Advanced Group Class provides a chance practice your skills with a diversity of moderately skilled partners.",
+        //   day: "Mondays",
+        //   rate: 10,
+        //   hasLink: true,
+        //   link: {
+        //     title: "Check the Calendar",
+        //     url: "/calendar",
+        //   },
+        // },
         {
           imgUrl:
             "https://firebasestorage.googleapis.com/v0/b/ballroom-east.appspot.com/o/public%2Fsocial.jpg?alt=media&token=d6983cfe-c1f7-4d4e-b929-c4a4499fc061",
