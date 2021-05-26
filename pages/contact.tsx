e686629cfb7b7c2537e19315279c7fdd39e746e2
Justin Chiou <chiou.kai@gmail.com>
2021-05-26 10:40:58 -0700

    feat: Contact Us page

---
+++ pages/contact.tsx
@@ -0,0 +1,16 @@
+const ContactUsPage = () => {
+  return (
+    <div className="container pt-3 py-md-5 text-center">
+      <h1>Contact Us!</h1>
+      <p>
+        Phone: <a href="tel:5023964207">(502) 396-4207</a>
+      </p>
+      <p>
+        Email:{" "}
+        <a href="mailto:linda@ballroomeast.com">linda@ballroomeast.com</a>
+      </p>
+    </div>
+  );
+};
+
+export default ContactUsPage;
