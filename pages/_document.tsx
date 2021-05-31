b7e463c62c3b1f41cba2d9320b1dd3b975967e04
Justin Chiou <chiou.kai@gmail.com>
2021-05-30 21:47:23 -0700

    feat: add gAnalytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,27 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const NEXT_PUBLIC_GOOGLE_ANALYTICS = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS}"
+          ></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', ${NEXT_PUBLIC_GOOGLE_ANALYTICS});`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
