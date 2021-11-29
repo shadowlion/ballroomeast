9a9a509af70f63ca3d23cfbc23de5d8089a38229
Justin Chiou <chiou.kai@gmail.com>
2021-11-29 12:09:44 -0500

    refactor: remove react-bootstrap from lessons page

--- pages/lessons.tsx
+++ pages/lessons.tsx
@@ -1,7 +1,11 @@
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
-import { Card, Jumbotron } from "react-bootstrap";
 import Layout from "../components/Layout";
+import BeginnerImg from "../public/img/beginner.jpg";
+import SocialImg from "../public/img/social.jpg";
+import PrivateImg from "../public/img/private.jpg";
+import CustomImg from "../public/img/custom.jpg";
 
 const LessonsPage = () => {
   return (
@@ -12,35 +16,37 @@ const LessonsPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <article id="lessons">
-        <section>
-          <Jumbotron className="mb-0" fluid>
-            <div className="container">
-              <h1 className="text-center display-3">Lessons</h1>
-              <hr className="my-5" />
-              <div className="text-muted text-justify lead">
-                <p>
-                  All of our lessons have a purpose. To have fun and at the same
-                  time transform the dancing into something great, whether for
-                  social dancing or competitive dancing.
-                </p>
-                <p>
-                  A dance partner is not required for any of these lessons.
-                  There is no up-front financial commitment, and you only pay
-                  for the classes you attend.
-                </p>
-              </div>
+        <section style={{ backgroundColor: "#e9ecef" }}>
+          <div className="container py-5">
+            <h1 className="text-center display-3">Lessons</h1>
+            <hr className="my-5" />
+            <div className="text-muted text-justify lead">
+              <p>
+                All of our lessons have a purpose. To have fun and at the same
+                time transform the dancing into something great, whether for
+                social dancing or competitive dancing.
+              </p>
+              <p>
+                A dance partner is not required for any of these lessons. There
+                is no up-front financial commitment, and you only pay for the
+                classes you attend.
+              </p>
             </div>
-          </Jumbotron>
+          </div>
         </section>
         <section className="bg-light">
           <div className="container py-5">
             <div className="row">
               <div className="col-12 col-md-6 pb-2">
-                <Card>
-                  <Card.Img variant="top" src="/img/beginner.jpg" />
-                  <Card.Body>
-                    <Card.Title>Group - Beginner</Card.Title>
-                    <Card.Text>
+                <div className="card">
+                  <Image
+                    src={BeginnerImg}
+                    alt="image for beginner classes"
+                    className="card-img-top"
+                  />
+                  <section className="card-body">
+                    <h5 className="card-title">Group - Beginner</h5>
+                    <p className="card-text">
                       All Beginner Group Classes are open to the public and
                       usually consists of 16 to 55 people. Beginners especially
                       like the opportunity to meet and dance with other novice
@@ -48,8 +54,8 @@ const LessonsPage = () => {
                       frequently throughout the group lesson (or stay with one
                       partner if they wish - no pressure!). This lesson is the
                       perfect blend of instruction and social ambiance.
-                    </Card.Text>
-                    <Card.Text className="text-muted">
+                    </p>
+                    <div className="card-text text-muted">
                       <address>
                         <strong>Wednesdays:</strong>
                         <br />
@@ -59,25 +65,31 @@ const LessonsPage = () => {
                         <br />
                         <span>$10/person</span>
                       </address>
-                    </Card.Text>
-                    <Card.Link className="mb-0 text-muted" href="/calendar">
-                      Check the Calendar &gt;&gt;
-                    </Card.Link>
-                  </Card.Body>
-                </Card>
+                    </div>
+                    <Link href="/calendar">
+                      <a className="card-link mb-0 text-muted">
+                        Check the Calendar &gt;&gt;
+                      </a>
+                    </Link>
+                  </section>
+                </div>
               </div>
               <div className="col-12 col-md-6 pb-2">
-                <Card>
-                  <Card.Img variant="top" src="/img/social.jpg" />
-                  <Card.Body>
-                    <Card.Title>Group - Social Party</Card.Title>
-                    <Card.Text>
+                <div className="card">
+                  <Image
+                    src={SocialImg}
+                    alt="image for social classes"
+                    className="card-img-top"
+                  />
+                  <section className="card-body">
+                    <h5 className="card-title">Group - Social Party</h5>
+                    <p className="card-text">
                       Every so often, we host Friday night social dance parties.
                       These nights normally consist of a Beginner Class (we
                       don&apos;t teach the same dance that happens on
                       Wednesdays), followed by some social dancing.
-                    </Card.Text>
-                    <Card.Text className="text-muted">
+                    </p>
+                    <div className="card-text text-muted">
                       <address>
                         <strong>Fridays:</strong>
                         <br />
@@ -89,18 +101,23 @@ const LessonsPage = () => {
                         <br />
                         <span>$10/person</span>
                       </address>
-                    </Card.Text>
-                    <Card.Link className="mb-0 text-muted" href="/calendar">
-                      Check the Calendar &gt;&gt;
-                    </Card.Link>
-                  </Card.Body>
-                </Card>
+                    </div>
+                    <Link href="/calendar">
+                      <a className="card-link mb-0 text-muted">
+                        Check the Calendar &gt;&gt;
+                      </a>
+                    </Link>
+                  </section>
+                </div>
               </div>
               <div className="col-12 col-md-6 pb-2">
-                <Card>
-                  <Card.Img variant="top" src="/img/private.jpg" />
-                  <Card.Body>
-                    <Card.Title>Private Lessons</Card.Title>
+                <div className="card">
+                  <Image
+                    src={PrivateImg}
+                    alt="image private lessons"
+                    className="card-img-top"
+                  />
+                  <section className="card-body">
                     <p className="card-text">
                       Private lessons are available for dancers of all ages and
                       experience levels. This is arguably the best way to become
@@ -108,7 +125,7 @@ const LessonsPage = () => {
                       you can decide which are the best mix of dances to learn
                       and which skillsets are best for your level of dance.
                     </p>
-                    <p className="card-text text-muted">
+                    <div className="card-text text-muted">
                       <address>
                         <strong>Rate:</strong>
                         <br />
@@ -118,8 +135,8 @@ const LessonsPage = () => {
                         <br />
                         <span>$110/60 minutes</span>
                       </address>
-                    </p>
-                    <p className="card-text text-muted">
+                    </div>
+                    <div className="card-text text-muted">
                       <address>
                         <strong>
                           Note: Starting January 1st, 2022, the rates for
@@ -132,31 +149,37 @@ const LessonsPage = () => {
                         <br />
                         <span>$120/60 minutes</span>
                       </address>
+                    </div>
+                    <Link href="/contact">
+                      <a className="card-link mb-0 text-muted">
+                        Contact Us to schedule &gt;&gt;
+                      </a>
+                    </Link>
+                  </section>
+                </div>
+              </div>
+              <div className="col-12 col-md-6 pb-2">
+                <div className="card">
+                  <Image
+                    src={CustomImg}
+                    alt="image for custom services"
+                    className="card-img-top"
+                  />
+                  <section className="card-body">
+                    <h5 className="card-title">Customized Services</h5>
+                    <p className="card-text">
+                      Out-of-town coaching, private wedding party/group lessons,
+                      classes for kids - if you need something more tailored to
+                      your liking, contact us and we&apos;ll work with you to
+                      getting that set up.
                     </p>
                     <Link href="/contact">
                       <a className="card-link mb-0 text-muted">
                         Contact Us to schedule &gt;&gt;
                       </a>
                     </Link>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="col-12 col-md-6 pb-2">
-                <Card>
-                  <Card.Img variant="top" src="/img/custom.jpg" />
-                  <Card.Body>
-                    <Card.Title>Customized Services</Card.Title>
-                    <Card.Text>
-                      Out-of-town coaching, private wedding party/group lessons,
-                      classes for kids - if you need something more tailored to
-                      your liking, contact us and we&apos;ll work with you to
-                      getting that set up.
-                    </Card.Text>
-                    <Card.Link className="mb-0 text-muted" href="/contact">
-                      Contact Us to schedule &gt;&gt;
-                    </Card.Link>
-                  </Card.Body>
-                </Card>
+                  </section>
+                </div>
               </div>
             </div>
           </div>
