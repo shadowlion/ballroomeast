fd380925e0720a0f46895013512ca42327c816cc
Justin Chiou <chiou.kai@gmail.com>
2021-11-29 12:41:15 -0500

    refactor: remove react-bootstrap from header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,49 +1,74 @@
 import Image from "next/image";
-import { Navbar, Nav } from "react-bootstrap";
+import Link from "next/link";
+import Logo from "../public/logo.svg";
 
 const Header = () => {
   return (
     <header>
-      <Navbar expand="sm" bg="dark" variant="dark">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
         <div className="container">
-          <Navbar.Brand href="/">
-            <Image
-              src="/logo.svg"
-              alt="Ballroom East Logo"
-              width="60px"
-              height="60px"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="nav-collapse" />
-          <Navbar.Collapse id="nav-collapse">
-            <Nav className="ml-auto text-center">
-              <Nav.Link href="/" className="text-white">
-                Home
-              </Nav.Link>
-              <Nav.Link href="/calendar" className="text-white">
-                Calendar
-              </Nav.Link>
-              <Nav.Link href="/lessons" className="text-white">
-                Lessons
-              </Nav.Link>
-              <Nav.Link href="/about" className="text-white">
-                About
-              </Nav.Link>
-              <Nav.Link href="/contact" className="text-white">
-                Contact
-              </Nav.Link>
-              <a
-                href="https://facebook.com/ballroomeast"
-                target="_blank"
-                rel="noreferrer"
-                className="nav-link text-white"
-              >
-                Facebook
-              </a>
-            </Nav>
-          </Navbar.Collapse>
+          <Link href="/">
+            <a className="navbar-brand">
+              <Image
+                src={Logo}
+                alt="Ballroom East Logo"
+                width="60px"
+                height="60px"
+              />
+            </a>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link href="/">
+                  <a className="nav-link text-light">Home</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/calendar">
+                  <a className="nav-link text-light">Calendar</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/lessons">
+                  <a className="nav-link text-light">Lessons</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/about">
+                  <a className="nav-link text-light">About</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/contact">
+                  <a className="nav-link text-light">Contact</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="https://facebook.com/ballroomeast"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-link text-white"
+                >
+                  Facebook
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </Navbar>
+      </nav>
     </header>
   );
 };
