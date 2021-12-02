080f1cc837b8947a56c564f4669afe3d68026217
Justin Chiou <chiou.kai@gmail.com>
2021-12-02 09:12:34 -0500

    fix: add bootstrap js to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 import "../style/global.css";
 
-export default function App({ Component, pageProps }) {
+const _App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return <Component {...pageProps} />;
-}
+};
+
+export default _App;
